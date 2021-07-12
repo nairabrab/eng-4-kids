8e57404e64da0bb4d0631ee234b25991a658a770
nairabrab <dan.kobarov@gmail.com>
2021-07-12 16:46:24 +0300

    feat: improve credits page

--- src/pages/credits.tsx
+++ src/pages/credits.tsx
@@ -4,6 +4,11 @@ import { useDispatch, useSelector } from 'react-redux'
 
 import { FullState, resetGameMode } from '../store'
 
+import happyImage from '../../public/assets/img/happy.jpg'
+import sadImage from '../../public/assets/img/sad.jpg'
+import failureSrc from '../../public/assets/audio/failure.mp3'
+import successSrc from '../../public/assets/audio/success.mp3'
+
 export default (): JSX.Element => {
   const dispatch = useDispatch()
   const history = useHistory()
@@ -18,10 +23,22 @@ export default (): JSX.Element => {
     }
   }, [dispatch, gameStatus, history])
 
+  useEffect(() => {
+    const playMatchSound = async () => {
+      // gameStatus ?
+      if (gameStatus === 'victory') await new Audio(successSrc).play()
+      if (gameStatus === 'game over') await new Audio(failureSrc).play()
+    }
+    void playMatchSound()
+    setTimeout(() => history.push('/'), 2000)
+  })
+  // [gameStatus, history]
+
   return (
     <div>
       <h1>Hello Stranger</h1>
       <h2>{gameStatus}</h2>
+      <img src={gameStatus === 'victory' ? happyImage : sadImage} alt='game statuss' />
     </div>
   )
 }
